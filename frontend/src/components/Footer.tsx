609faebc71d4bc8f4df9ea4008ecd0c68a3358b7
datanooblol <data.noob.lol@gmail.com>
2024-04-30 13:19:17 +0700

    restore icon

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,7 +10,14 @@ const Footer: React.FC = () => {
             <CloudIcon className="w-5 h-5 mr-1.5" />
             Powered by enstructure AI Solutions
           </div>
-
+          <div className="inline-flex items-center hover:underline underline-offset-2">
+            <img
+              src={GitHub}
+              alt="React Logo"
+              width={20}
+              className="mr-1.5 py-2 mx-2"
+            />
+          </div>
         </div>
       </footer>
     </div>
