a24e02ad324fe2acf379dea4d6e1f0277a676cf0
datanooblol <data.noob.lol@gmail.com>
2024-04-30 13:09:25 +0700

    remove github icon

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,13 +10,13 @@ const Footer: React.FC = () => {
             <CloudIcon className="w-5 h-5 mr-1.5" />
             Powered by enstructure AI Solutions
           </div>
-          <div className="inline-flex items-center hover:underline underline-offset-2">
+          {/* <div className="inline-flex items-center hover:underline underline-offset-2">
             <img
               src={GitHub}
               alt="React Logo"
               width={20}
               className="mr-1.5 py-2 mx-2"
-            />
+            /> */}
           </div>
         </div>
       </footer>
